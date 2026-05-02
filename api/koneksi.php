0b3a0630c3896ef07fe80c7fddefbde9121be341
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-05-02 14:33:40 +0700

    first commit

---
+++ api/koneksi.php
@@ -0,0 +1,23 @@
+<?php 
+
+$host     = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com'
+$username = '28bs2jCJxPBCHff.root'
+$password = 'xGUugfYqG11ci1bk'
+$database = 'warteg-indo'
+$port     = 4000;
+
+try{
+    $dsn = "mysql:host=$host; port=$port; dbname=$database; username=$username; password=$password";
+    $connection = new PDO($dsn, $username, $password, [
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+        PDO::MYSQL_ATTR_SSL_CA => '/etc/ssl/certs/ca-certificates.crt',
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_TIMEOUT => 8,
+    ]);
+
+}catch(PDOException $error){
+    http_response_code(500);
+    die("DB Error: ". $error->getMessage());
+}
+
+
