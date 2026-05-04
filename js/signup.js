1b98612da1338b2fe4fd7240da22b9b891d6a6ed
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-05-04 16:19:10 +0700

    edit signup js

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-    $("#btnSignUp").click(function(){
+    $("#btnSignUp").click(function(e){
+        e.preventDefault();
+
         var nama     = $("#idNama").val();
         var email    = $("#idEmail").val();
         var password = $("#idPassword").val();
 
-        var data = {
-            nama:nama,
-            email: email,
-            password: password
-        };
+        console.log("Nama:", nama);
+        console.log("Email:", email);
+        console.log("Password:", password);
 
+        if(!nama || !email || !password){
+            alert('Semua field harus diisi!');
+            return;
+        }
+
+        var data = { nama: nama, email: email, password: password };
         var url = "/proses/prosesSignUp.php";
 
+        console.log("Kirim ke:", url);
+        console.log("Data:", data);
+
         ajax.ajaxPost(url, data, function(result){
+            console.log("Response:", result);
             if(result.status === false){
                 alert(result.message);
-            }else{
-                window.location.href="/login";
+            } else {
+                window.location.href = "/login";
             }
         });
     });
