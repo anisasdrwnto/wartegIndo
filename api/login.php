cd9a1c8abf0f89073881cb4d246bcdee2f7f070b
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-05-04 09:27:31 +0700

    add login js dan edit style login

--- api/login.php
+++ api/login.php
@@ -4,8 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="/js/login.js"></script>
+    <script src="jquery-3.7.1.min.js"></script>
 </head>
 <body>
-    <p>Ini Login</p>
+   <form>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email Address</label>
+        <input type="email" class="form-control" id="idEmail" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" class="form-control" id="idPassword">
+    </div>
+    <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="idCheck">
+        <label class="form-check-label" for="checking">Remember Me</label>
+    </div>
+    <button type="button" class="btn btn-primary">Submit</button>
+</form>
 </body>
 </html>
