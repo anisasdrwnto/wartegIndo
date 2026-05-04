d02b093c90f3a2eb47d0dd20a840ae35d96df60c
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-05-04 10:07:49 +0700

    add global js

---
+++ global.js
@@ -0,0 +1,79 @@
+console.log("global js");
+var base_url = window.location.origin;
+console.log(base_url);
+
+var ajax = {
+    ajaxTest:function(json){
+        console.log("Test ajax");
+        console.log(json);
+    },
+    ajaxGet:function (url, data, next){
+        console.log(base_url+url);
+        $.ajax({
+            // method : "GET",
+            type: "GET",
+            url: base_url + url,
+            data: data,
+            dataType: "json",
+            success: function (result) {
+                next(result);
+            },
+            error: function(err){
+                var msg = "Error, Please contact the Administrator";
+                var data = {
+                    err : err,
+                    msgErr : msg
+                }
+                next(data);
+            }
+        });
+    },
+    ajaxPost:function (url, data, next){
+        console.log(base_url+url);
+        $.ajax({
+            // method : "GET",
+            type: "POST",
+            url: base_url + url,
+            data: data,
+            dataType: "json",
+            success: function (result) {
+                next(result);
+            },
+            error: function(err){
+                var msg = "Error, Please contact the Administrator";
+                var data = {
+                    err : err,
+                    msgErr : msg
+                }
+                next(data);
+            }
+        });
+    },
+    ajaxPostWithFile:function (url, data, next){
+        $.ajax({
+            method: "POST",
+            url: base_url + url,
+            data: data,
+            dataType: "json",
+            contentType: false,
+            processData: false,
+            success: function (result) {
+                next(result);
+            },
+            error: function(err){
+                var msg = "Error, Please contact the Administrator";
+                var data = {
+                    err : err,
+                    msgErr : msg
+                }
+                next(data);
+            }
+        });
+    }
+}
+
+$(document).ready(function(){
+    $("#logout").click(function(){
+        window.location.href = base_url+"/logout";
+    });
+});
--- js/login.js
+++ js/login.js
@@ -1,12 +1,7 @@
 $(document).ready(function(){
-    // console.log("ini login js");
-
     $("#btnLogin").click(function(){
-        alert('test button login');
-        // e.preventDefault();
-        // var email    = $("#idEmail").val();
-        // var password = $("#idPassword").val();
-        // console.log(email);
-        // console.log(password);
+        var email    = $("#idEmail").val();
+        var password = $("#idPassword").val();
+        
     });
 });
