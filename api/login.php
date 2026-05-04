2c7b3b71ff39d49a9dba228f25c805a615a06c6c
anisasdrwnto <anisasudarwanto1@gmail.com>
2026-05-04 09:43:45 +0700

    fixing style login

--- api/login.php
+++ api/login.php
@@ -10,21 +10,25 @@
     <script src="/js/login.js"></script>
 </head>
 <body>
-   <form>
-    <div class="mb-3">
-        <label for="email" class="form-label">Email Address</label>
-        <input type="email" class="form-control" id="idEmail" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="col-md-6 col-lg-4">
+            <form class="p-4 border rounded bg-light">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email Address</label>
+                <input type="email" class="form-control" id="idEmail" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="idPassword">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="idCheck">
+                <label class="form-check-label" for="checking">Remember Me</label>
+            </div>
+            <button type="button" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
     </div>
-    <div class="mb-3">
-        <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="idPassword">
-    </div>
-    <div class="mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="idCheck">
-        <label class="form-check-label" for="checking">Remember Me</label>
-    </div>
-    <button type="button" class="btn btn-primary">Submit</button>
-</form>
 </body>
 </html>
